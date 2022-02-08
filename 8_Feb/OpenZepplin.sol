11ba47a795f2e7008767aa2c28517404d2c34aec
prempatel5670 <98372711+prempatel5670@users.noreply.github.com>
2022-02-08 18:24:44 +0530

    Create OpenZepplin.sol

---
+++ 8_Feb/OpenZepplin.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts@4.2.0/token/ERC20/ERC20.sol";
+//import the ERC20 contract
+contract openzeppelin is ERC20{
+    constructor(uint initialsupply) ERC20("HELLO","XYZ"){
+        _mint(msg.sender,initialsupply);
+    
+    }
+    //destroy the tokens
+    function burn(uint256 amount)public {
+        _burn(msg.sender,amount);
+
+    }
+}
